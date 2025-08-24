94a905789cc1125cb13e0b53f9790a1f14e70722
Rishabh567892 <146851398+Rishabh567892@users.noreply.github.com>
2025-08-24 13:56:48 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const tasksRouter = require("./routes/tasks.routes")
 const { authenticateToken, verfiyTask } = require("./middlewares/auth.middleware");
 
 app.use(cors({
-  origin: "https://taskshower.netlify.app/",
+  origin: "*",
   methods: "*"
 }))
 
